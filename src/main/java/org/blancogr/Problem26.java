0b6ffdf6def91f0e3deb76ffc69c26582720d97c
Gerardo Blanco <blancogr@familysearch.org>
2017-12-04 16:52:48 -0700

    Problem 26

---
+++ src/main/java/org/blancogr/Problem26.java
@@ -0,0 +1,45 @@
+package org.blancogr;
+
+import java.math.BigInteger;
+
+import static java.lang.Math.log;
+
+/**
+ * A unit fraction contains 1 in the numerator.
+ * The decimal representation of the unit fractions with denominators 2 to 10 are given:
+ *
+ *  1/2	= 	0.5
+ *  1/3	= 	0.(3)
+ *  1/4	= 	0.25
+ *  1/5	= 	0.2
+ *  1/6	= 	0.1(6)
+ *  1/7	= 	0.(142857)
+ *  1/8	= 	0.125
+ *  1/9	= 	0.(1)
+ *  1/10	= 	0.1
+ *  Where 0.1(6) means 0.166666..., and has a 1-digit recurring cycle.
+ *  It can be seen that 1/7 has a 6-digit recurring cycle.
+ *
+ *  Find the value of d < 1000 for which 1/d contains the longest recurring cycle
+ *  in its decimal fraction part.
+ */
+
+
+public class Problem26 {
+    public static void main(String[] args) {
+        mine();
+        //theirs();
+    }
+
+    public static void mine() {
+        for(int i = 1; i < 10; i++) {
+            System.out.println(i + " " + 1000 % i + " " + 1.0f / i);
+        }
+    }
+
+    public static void theirs() {
+        Double phi = (1 + Math.sqrt(5)) / 2;
+        Double lowerBound = (999 * log(10) + log(5) / 2) / log(phi);
+        System.out.println("Result (theirs) is: " + Math.round(lowerBound));
+    }
+}
